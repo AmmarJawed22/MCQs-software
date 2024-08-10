cb9ca20373d8c673a9046077276aa39ff3c794f9
AmmarJawed22 <105720370+AmmarJawed22@users.noreply.github.com>
2024-08-09 23:36:51 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using McQS.data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddMvc();
+builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(
+     builder.Configuration.GetConnectionString("connection")
+     ));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=fetchQuiz}");
+
+app.Run();
